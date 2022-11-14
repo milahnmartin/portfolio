8a34509b418123afd77789a46ac261a3a1d71965
milahnmartin <milahnmartin.develop@gmail.com>
2022-11-14 14:05:08 +0200

    feat(socials)

--- src/components/SidebarLeft.tsx
+++ src/components/SidebarLeft.tsx
@@ -1,12 +1,38 @@
-import { AiFillGithub } from "react-icons/ai";
+import {
+  AiFillGithub,
+  AiFillInstagram,
+  AiFillLinkedin,
+  AiFillTwitterCircle,
+} from "react-icons/ai";
 const SidebarLeft = () => {
   return (
     <div className='flex flex-col space-y-6'>
-      <AiFillGithub size={30} fill='#495670' />
-      <AiFillGithub size={30} fill='#495670' />
-      <AiFillGithub size={30} fill='#495670' />
-      <AiFillGithub size={30} fill='#495670' />
-      <AiFillGithub size={30} fill='#495670' />
+      <AiFillGithub
+        className='cursor-pointer hover:scale-110 '
+        size={30}
+        fill='#495670'
+        onClick={() => window.open("https://github.com/milahnmartin")}
+      />
+      <AiFillLinkedin
+        className='cursor-pointer hover:scale-110 '
+        size={30}
+        fill='#495670'
+        onClick={() =>
+          window.open("https://www.linkedin.com/in/milahn-martin-376416203/")
+        }
+      />
+      <AiFillTwitterCircle
+        className='cursor-pointer hover:scale-110 '
+        size={30}
+        fill='#495670'
+        onClick={() => window.open("https://twitter.com/Ultrafyy")}
+      />
+      <AiFillInstagram
+        className='cursor-pointer hover:scale-110'
+        size={30}
+        fill='#495670'
+        onClick={() => window.open("https://www.instagram.com/milahnmartin/")}
+      />
     </div>
   );
 };
