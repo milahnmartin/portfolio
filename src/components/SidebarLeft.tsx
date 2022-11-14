3f23ddde441cb49c28c0a29f52ac7c2009af8abd
milahnmartin <milahnmartin.develop@gmail.com>
2022-11-15 00:32:30 +0200

    fix(sidebar&hover)

--- src/components/SidebarLeft.tsx
+++ src/components/SidebarLeft.tsx
@@ -8,13 +8,13 @@ const SidebarLeft = () => {
   return (
     <div className='flex flex-col space-y-6'>
       <AiFillGithub
-        className='cursor-pointer hover:scale-110 '
+        className='cursor-pointer hover:scale-110 transition-all duration-100'
         size={30}
         fill='#495670'
         onClick={() => window.open("https://github.com/milahnmartin")}
       />
       <AiFillLinkedin
-        className='cursor-pointer hover:scale-110 '
+        className='cursor-pointer hover:scale-110 transition-all duration-100'
         size={30}
         fill='#495670'
         onClick={() =>
@@ -22,13 +22,13 @@ const SidebarLeft = () => {
         }
       />
       <AiFillTwitterCircle
-        className='cursor-pointer hover:scale-110 '
+        className='cursor-pointer hover:scale-110 transition-all duration-100'
         size={30}
         fill='#495670'
         onClick={() => window.open("https://twitter.com/Ultrafyy")}
       />
       <AiFillInstagram
-        className='cursor-pointer hover:scale-110'
+        className='cursor-pointer hover:scale-110 transition-all duration-100'
         size={30}
         fill='#495670'
         onClick={() => window.open("https://www.instagram.com/milahnmartin/")}
