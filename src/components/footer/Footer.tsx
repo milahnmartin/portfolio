3ff449b65c353568066f328d11cd184a1540db86
milahnmartin <milahnmartin.develop@gmail.com>
2023-01-02 14:14:38 +0200

    fix(footer):Copyright

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -22,7 +22,7 @@ const Footer = () => {
         onClick={handleFooterClick}
         className='group text-text/60 cursor-pointer hover:text-secondary animation-all duration-500 space-y-2 flex flex-col justify-center items-center'
       >
-        Built by Milahn Martin &copy; 2022 <br />{" "}
+        Built by Milahn Martin &copy; {new Date().getFullYear()} <br />{" "}
         <VscGithubAlt className='group-hover:text-secondary group-hover:animate-pulse animation-all duration-500' />
         Git Repos: {githubdata}
       </h1>
