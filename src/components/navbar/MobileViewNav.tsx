9945f3c74c7be103736315816a96b4a4f8036032
milahnmartin <milahnmartin.develop@gmail.com>
2023-01-31 17:49:55 +0200

    feat(resume):Added New Resume

--- src/components/navbar/MobileViewNav.tsx
+++ src/components/navbar/MobileViewNav.tsx
@@ -1,73 +1,73 @@
-import { useState } from "react";
-import { AiOutlineClose } from "react-icons/ai";
-import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from 'react';
+import { AiOutlineClose } from 'react-icons/ai';
+import { GiHamburgerMenu } from 'react-icons/gi';
 const MobileViewNav = () => {
   const [open, setOpen] = useState<boolean>(false);
   const scrollToComponent = (component: string) => {
     const element = document.getElementById(component);
-    element?.scrollIntoView({ behavior: "smooth" });
+    element?.scrollIntoView({ behavior: 'smooth' });
     setOpen(false);
   };
   return (
-    <div className='h-screen w-screen flex items-start justify-end'>
+    <div className="h-screen w-screen flex items-start justify-end">
       {open ? (
         <AiOutlineClose
-          id='close-hamburger'
+          id="close-hamburger"
           onClick={() => setOpen((prev: boolean) => !prev)}
-          className='cursor-pointer z-50'
+          className="cursor-pointer z-50"
           size={30}
-          fill='#64ffda'
+          fill="#64ffda"
         />
       ) : (
         <GiHamburgerMenu
-          id='hamburger-menu'
+          id="hamburger-menu"
           onClick={() => setOpen((prev: boolean) => !prev)}
-          className='cursor-pointer z-50  text-secondary'
+          className="cursor-pointer z-50  text-secondary"
           size={30}
-          fill='#64ffda'
+          fill="#64ffda"
         />
       )}
       {open && (
-        <div className='h-screen w-screen flex flex-col items-center p-2 justify-center text-center text-text bg-primary m-auto absolute top-0 right-0 space-y-16'>
+        <div className="h-screen w-screen flex flex-col items-center p-2 justify-center text-center text-text bg-primary m-auto absolute top-0 right-0 space-y-16">
           <h1
-            onClick={() => scrollToComponent("about")}
-            className='hover:text-secondary cursor-pointer animation-all duration-300 w-[35%] flex items-center justify-start tracking-widest'
+            onClick={() => scrollToComponent('about')}
+            className="hover:text-secondary cursor-pointer animation-all duration-300 w-[35%] flex items-center justify-start tracking-widest"
           >
-            <span className='text-secondary w-[20%] flex justify-start tracking-normal'>
+            <span className="text-secondary w-[20%] flex justify-start tracking-normal">
               01.
             </span>
-            <span className='w-full'>About</span>
+            <span className="w-full">About</span>
           </h1>
           <h1
-            onClick={() => scrollToComponent("projects")}
-            className='hover:text-secondary cursor-pointer animation-all duration-300 w-[35%] flex items-center justify-center'
+            onClick={() => scrollToComponent('projects')}
+            className="hover:text-secondary cursor-pointer animation-all duration-300 w-[35%] flex items-center justify-center"
           >
-            <span className='text-secondary w-[20%] flex justify-start tracking-normal'>
+            <span className="text-secondary w-[20%] flex justify-start tracking-normal">
               02.
             </span>
-            <span className='w-full'>Projects</span>
+            <span className="w-full">Projects</span>
           </h1>
           <h1
-            onClick={() => scrollToComponent("education")}
-            className='hover:text-secondary cursor-pointer animation-all duration-300 w-[35%] flex items-center justify-center tracking-widest'
+            onClick={() => scrollToComponent('education')}
+            className="hover:text-secondary cursor-pointer animation-all duration-300 w-[35%] flex items-center justify-center tracking-widest"
           >
-            <span className='text-secondary w-[20%] flex justify-start tracking-normal'>
+            <span className="text-secondary w-[20%] flex justify-start tracking-normal">
               03.
             </span>
-            <span className='w-full'>Education</span>
+            <span className="w-full">Education</span>
           </h1>
           <h1
-            onClick={() => scrollToComponent("contacts")}
-            className='hover:text-secondary cursor-pointer animation-all duration-300 w-[35%] flex items-center justify-center tracking-widest'
+            onClick={() => scrollToComponent('contacts')}
+            className="hover:text-secondary cursor-pointer animation-all duration-300 w-[35%] flex items-center justify-center tracking-widest"
           >
-            <span className='text-secondary w-[20%] flex justify-start tracking-normal'>
+            <span className="text-secondary w-[20%] flex justify-start tracking-normal">
               04.
             </span>
-            <span className='w-full'>Contact</span>
+            <span className="w-full">Contact</span>
           </h1>
           <button
-            onClick={() => window.open("/milahn-martin-resume.pdf")}
-            className='px-4 py-2 outline-none animation-all duration-300 border-[1px] w-[50%] border-secondary rounded-md text-secondary hover:bg-secondary/10'
+            onClick={() => window.open('/Milahn_Martin_-_Honours_Student.pdf')}
+            className="px-4 py-2 outline-none animation-all duration-300 border-[1px] w-[50%] border-secondary rounded-md text-secondary hover:bg-secondary/10"
           >
             Resume
           </button>
--- src/components/navbar/NormalViewNav.tsx
+++ src/components/navbar/NormalViewNav.tsx
@@ -1,15 +1,15 @@
 const NormalViewNav = () => {
   const scrollToComponent = (component: string) => {
     const element = document.getElementById(component);
-    element?.scrollIntoView({ behavior: "smooth" });
+    element?.scrollIntoView({ behavior: 'smooth' });
   };
   return (
-    <div className='h-full w-full flex space-x-6 items-center justify-end text-text'>
+    <div className="h-full w-full flex space-x-6 items-center justify-end text-text">
       <h1
-        onClick={() => scrollToComponent("about")}
-        className='hover:text-secondary cursor-pointer animation-all duration-300'
+        onClick={() => scrollToComponent('about')}
+        className="hover:text-secondary cursor-pointer animation-all duration-300"
       >
-        <span className='text-secondary'>01.</span>About
+        <span className="text-secondary">01.</span>About
       </h1>
       {/* <h1
         onClick={() => scrollToComponent("experience")}
@@ -18,27 +18,27 @@ const NormalViewNav = () => {
         <span className='text-secondary'>02.</span>Experience
       </h1> */}
       <h1
-        onClick={() => scrollToComponent("projects")}
-        className='hover:text-secondary cursor-pointer animation-all duration-300'
+        onClick={() => scrollToComponent('projects')}
+        className="hover:text-secondary cursor-pointer animation-all duration-300"
       >
-        <span className='text-secondary'>02.</span>Projects
+        <span className="text-secondary">02.</span>Projects
       </h1>
       <h1
-        onClick={() => scrollToComponent("education")}
-        className='hover:text-secondary cursor-pointer animation-all duration-300'
+        onClick={() => scrollToComponent('education')}
+        className="hover:text-secondary cursor-pointer animation-all duration-300"
       >
-        <span className='text-secondary'>03.</span>Education
+        <span className="text-secondary">03.</span>Education
       </h1>
       <h1
-        onClick={() => scrollToComponent("contacts")}
-        className='hover:text-secondary cursor-pointer animation-all duration-300'
+        onClick={() => scrollToComponent('contacts')}
+        className="hover:text-secondary cursor-pointer animation-all duration-300"
       >
-        <span className='text-secondary'>04.</span>Contact
+        <span className="text-secondary">04.</span>Contact
       </h1>
 
       <button
-        onClick={() => window.open("/milahn-martin-resume.pdf")}
-        className='px-4 py-2 outline-none animation-all duration-300 border-[1px] border-secondary rounded-md text-secondary hover:bg-secondary/10'
+        onClick={() => window.open('/Milahn_Martin_-_Honours_Student.pdf')}
+        className="px-4 py-2 outline-none animation-all duration-300 border-[1px] border-secondary rounded-md text-secondary hover:bg-secondary/10"
       >
         Resume
       </button>
