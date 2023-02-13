0332374e038c31e65ed3ccd94b2e0e7b56a69f88
milahnmartin <milahnmartin.develop@gmail.com>
2023-02-13 14:18:50 +0200

    feat(education): Updated Honours

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -8,11 +8,19 @@ type EducationData = {
 const MyEducation: EducationData[] = [
   {
     college: 'Eduvos',
-    degree: 'BSC IT',
+    degree: 'BSC IT (SWE)',
     grade: 'Cum Laude (90%)',
     year_finished: new Date('2022-12-9'),
     description:
       'Finished My Bachelors Degree in Information Technology Majoring in Software Engineering in 2022 and will be graduating with a Cum Laude (90%), thereafter I will continue with my studies in 2023 when I pursue a Honours degree.',
+  },
+  {
+    college: 'Eduvos',
+    degree: 'BSC IT (SWE) Honours',
+    grade: 'Present',
+    year_finished: new Date('2023-12-8'),
+    description:
+      'Currently (2023) continuing my studying in the field of IT and will be done at the end of 2023.',
   },
 ];
 
@@ -27,7 +35,7 @@ function Education() {
         <hr className="w-[10rem] text-text2" />
       </h1>
       <div className="h-fit w-full block">
-        {MyEducation!.map((education: EducationData) => {
+        {MyEducation.map((education: EducationData) => {
           return <EducationLabel {...education} />;
         })}
       </div>
@@ -41,7 +49,7 @@ import { IoSchoolOutline } from 'react-icons/io5';
 import { MdOutlineGrade, MdOutlineRotate90DegreesCcw } from 'react-icons/md';
 function EducationLabel(props: EducationData) {
   return (
-    <div className="h-fit bg-navy2 rounded-lg px-4 py-4 md:h-[15rem] project-view">
+    <div className="h-fit bg-navy2 rounded-lg px-4 py-4 md:h-[15rem] project-view mb-5">
       <div className="flex flex-col items-center w-full justify-center text-center text-text md:space-x-8 md:flex-row md:h-[10rem] md:justify-center md:items-center">
         <h1 className="flex items-center justify-center gap-2 text-2xl">
           <IoSchoolOutline
@@ -72,7 +80,7 @@ function EducationLabel(props: EducationData) {
             />{' '}
             Completed:
           </span>{' '}
-          {props.year_finished?.toDateString() || 'Fri Dec 09 2022'}
+          {props.year_finished?.toDateString() || 'Cant Parse Date :)'}
         </h1>
       </div>
       <p className="text-text2 text-lg hover:text-secondary cursor-pointer transition-colors duration-700 text-center w-full">
